fb4646903b677446af59cb9f049ab3bb6baf8017
ntargon <argon19951128@gmail.com>
2023-11-26 10:54:11 +0900

    feat: サンプルコードを修正

--- src/main.cpp
+++ src/main.cpp
@@ -6,18 +6,66 @@
 
 #include <stdio.h>
 
-static StaticTask_t exampleTaskTCB;
-static StackType_t exampleTaskStack[ configMINIMAL_STACK_SIZE ];
+/*
+FreeRTOSのAPIでt-kernelやuITRONのメッセージバッファを模擬するには
+
+## 案1
+Queueとmutexを用意する。
+書き込み時には、mutexを獲得する。
+mutex取得できないならまち。
+獲得できたら、書き込む。キューがいっぱいならここでも待ちが発生する。
+その後mutexの開放。
+
+デメリットは、タイムアウトの指定が同じようにできないところ
+mutexの待ちとキューの待ちがダブルで乗る可能性がある
+
+## 案2
+Queueとmutexとcounting semaphoreを用意する
+counting semaphoreの獲得。ここで待ちが発生する。
+優先度継承のmutexを獲得。ここでの待ち時間はほぼ0とみなせる。
+Queueに書き込み。カウンティングセマフォでキューに空きがあることは確定しているので、待ちは発生しない。
+mutexの開放。
+
+受信側で、受信後にcounting semaphoreを開放する。
+
+*/
+
+void highPriorityTask( void * parameters );
 
 void exampleTask( void * parameters )
 {
     /* Unused parameters. */
     ( void ) parameters;
 
+    int i = 0;
+
     for( ; ; )
     {
         /* Example Task Code */
-        vTaskDelay( 100 ); /* delay 100 ticks */
+        vTaskDelay( 1000 ); /* delay 1000 ticks */
+        printf("low: %d\n", i++);
+
+        if (i == 10) {
+            xTaskCreate( highPriorityTask,
+                            "high",
+                            configMINIMAL_STACK_SIZE,
+                            NULL,
+                            configMAX_PRIORITIES - 1,
+                            NULL);
+        }
+    }
+}
+
+
+void highPriorityTask( void * paramters )
+{
+    ( void ) paramters;
+
+    int i = 0;
+    for( ; ; )
+    {
+        vTaskDelay( 500 );
+        printf("high: %d\n", i++);
     }
 }
 
@@ -25,13 +73,12 @@ int main( void )
 {
     printf( "Example FreeRTOS Project\n" );
 
-    xTaskCreateStatic( exampleTask,
+    xTaskCreate( exampleTask,
                        "example",
                        configMINIMAL_STACK_SIZE,
                        NULL,
-                       configMAX_PRIORITIES - 1,
-                       exampleTaskStack,
-                       &exampleTaskTCB );
+                       configMAX_PRIORITIES - 2,
+                       NULL);
 
     /* Start the scheduler. */
     vTaskStartScheduler();
